4f79b42bfd4c89f0540e872d70c9a8cc768a2391
Sangharsh Seth <sethsangharsh@outlook.com>
2023-08-05 00:43:33 +0530

    some update

--- internal/util.go
+++ internal/util.go
@@ -2,12 +2,10 @@ package internal
 
 import (
 	"fmt"
-	"github.com/pieterclaerhout/go-log"
 	"net"
 )
 
 func SendRedisResponse(msg any, connection net.Conn) {
-	log.Info(msg)
 	response := fmt.Sprintf("+%s\r\n", msg)
 	_, err := connection.Write([]byte(response))
 	if err != nil {
